3bbd96293e2a767f566d712966206501c2a6efc8
MrLock1378 <rezafaghihi1379@gmail.com>
2017-06-17 21:27:46 +0430

    Ɓℓαcк ƁOƬ

--- plugins/fun.lua
+++ plugins/fun.lua
@@ -356,7 +356,7 @@ _You can use_ *[!/#]* _at the beginning of commands._
 else
 
 helpfun_en = [[
-_راهنمای فان ربات بیوند:_
+_راهنمای فان ربات بلک بوت:_
 
 *!time*
 _دریافت ساعت به صورت استیکر_
@@ -405,7 +405,7 @@ local hash = "gp_lang:"..msg.to.id
 local lang = redis:get(hash)
 if not lang then
 helpfun_fa = [[
-_Beyond Reborn Fun Help Commands:_
+_Ɓℓαcк ƁOƬ  Fun Help Commands:_
 
 *ساعت*
 _Get time in a sticker_
@@ -446,7 +446,7 @@ _Get weather_
 else
 
 helpfun_fa = [[
-_راهنمای فان ربات بیوند:_
+_راهنمای فان ربات بلک بوت:_
 
 *ساعت*
 _دریافت ساعت به صورت استیکر_
